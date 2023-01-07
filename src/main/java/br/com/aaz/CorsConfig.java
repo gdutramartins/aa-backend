03376e942ab1cb579359ed0a5fb4b005fc0d0433
gdutramartins <gdutramartins@gmail.com>
2023-01-07 19:15:24 -0300

    Cors

--- src/main/java/br/com/aaz/CorsConfig.java
+++ src/main/java/br/com/aaz/CorsConfig.java
@@ -1,19 +1,53 @@
 package br.com.aaz;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/*
+ * import org.springframework.context.annotation.Configuration; import
+ * org.springframework.web.servlet.config.annotation.CorsRegistry; import
+ * org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+ * 
+ * @Configuration public class CorsConfig implements WebMvcConfigurer {
+ * 
+ * @Override public void addCorsMappings(CorsRegistry registry) {
+ * 
+ * registry.addMapping("/**") //
+ * .allowedOrigins("https://amigo-azul-front.herokuapp.com")
+ * .allowedOriginPatterns("https://amigo-azul-front.herokuapp.com")
+ * .allowCredentials(true) .allowedMethods("*") .maxAge(3600L); } }
+ */
+
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
+public class CorsConfig {
+    
+    private CorsConfiguration buildConfig() {
+    
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        //corsConfiguration.addAllowedOrigin("*");
+        //  Cross domain configuration error , take .allowedOrigins Replace with .allowedOriginPatterns that will do .
+        //  Set the domain name that allows cross domain requests 
+        corsConfiguration.addAllowedOriginPattern("*");
+        corsConfiguration.addAllowedHeader("*");
+        //  Set allowed methods 
+        corsConfiguration.addAllowedMethod("*");
+        //  Whether to allow certificates 
+        corsConfiguration.setAllowCredentials(true);
+        //  Cross domain allow time 
+        corsConfiguration.setMaxAge(3600L);
+        return corsConfiguration;
+    }
 
-		registry.addMapping("/**")
-				// .allowedOrigins("https://amigo-azul-front.herokuapp.com")
-				.allowedOriginPatterns("https://amigo-azul-front.herokuapp.com")
-				.allowCredentials(true)
-				.allowedMethods("*")
-				.maxAge(3600L);
-	}
+    @Bean
+    CorsFilter corsFilter() {
+    
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig());
+        return new CorsFilter(source);
+    }
+
 }
